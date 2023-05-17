628db7c54495d2c6f14cc37d27ddd44444106886
devraysingh <raysahni98@gmail.com>
2023-05-17 12:12:13 -0400

    Initial Commit

---
+++ SwiftCoinTutorial/SwiftCoinTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinTutorialApp.swift
+//  SwiftCoinTutorial
+//
+//  Created by ray on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
